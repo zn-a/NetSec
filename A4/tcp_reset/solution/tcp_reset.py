e8f2b308d0d98ad7346893e81cbfd20f1e6a648b
Zubair Al-Zubi <a.n.f.al-zubi@student.tudelft.nl>
2025-05-23 12:23:57 +0200

    Implement usage function and main logic for TCP reset attack script

--- A4/tcp_reset/solution/tcp_reset.py
+++ A4/tcp_reset/solution/tcp_reset.py
@@ -1,4 +1,22 @@
 from scapy.all import *
 from sys import argv
 
-# ...
+
+def usage():
+    print("Usage: python3 tcp_reset.py <src_ip> <dst_ip> <dst_port>")
+    exit(1)
+
+
+def main():
+    if len(argv) != 4:
+        usage()
+
+    src_ip = argv[1]        # 192.168.124.20 (host2)
+    dst_ip = argv[2]        # 192.168.124.10 (host1)
+    dst_port = int(argv[3]) # dest port (1337)
+
+    print(f"Executing TCP-reset attack: from {src_ip} to {dst_ip}:{dst_port}")
+
+
+if __name__ == "__main__":
+    main()
